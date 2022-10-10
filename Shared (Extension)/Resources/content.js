e30c42a2d617a2dfdee481b3862d3ea671fcadf9
Daniel Nitsikopoulos <daniel@dnitza.com>
2022-10-10 23:03:35 +1100

    Initial commit

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,43 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+if (window.top === window) {
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+    function waitForElement(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    resolve(document.querySelector(selector));
+                    observer.disconnect();
+                }
+            });
+
+            observer.observe(document.body, {
+            childList: true,
+            subtree: true
+            });
+        });
+    }
+
+    const addCommentButtons = document.querySelectorAll("[data-type='addition']")
+
+    const submitButtonSelector = "button.btn.btn-primary > span.start-review-label"
+
+    addCommentButtons.forEach(addCommentButton => {
+        addCommentButton.addEventListener("click", function prReviewCheck(event) {
+
+            addCommentButton.removeEventListener('click', prReviewCheck);
+
+            waitForElement(submitButtonSelector).then(() => {
+                const button = document.querySelector(submitButtonSelector).closest("button")
+
+                button.addEventListener("click", function(e) {
+                    if(!window.confirm("Do you want to start a review?")) {
+                        e.preventDefault();
+                    }
+                });
+            });
+        });
+    });
+}
